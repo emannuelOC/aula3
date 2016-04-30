ff059aa7c32c15c13cc36f90b399c7ac7efa847a
Emannuel Fernandes de Oliveira Carvalho <emannuelcarvalho@macbook-air-de-rafael-feroli.local>
2016-04-30 09:55:57 -0300

    Initial Commit

---
+++ Miscellanea/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Miscellanea
+//
+//  Created by Emannuel Carvalho on 4/30/16.
+//  Copyright © 2016 Emannuel Carvalho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MiscellaneaTests/MiscellaneaTests.m
@@ -0,0 +1,39 @@
+//
+//  MiscellaneaTests.m
+//  MiscellaneaTests
+//
+//  Created by Emannuel Carvalho on 4/30/16.
+//  Copyright © 2016 Emannuel Carvalho. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MiscellaneaTests : XCTestCase
+
+@end
+
+@implementation MiscellaneaTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
